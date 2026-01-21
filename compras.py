3baa5d2af93b8e6c6046b459db4653a2f2d20d31
Marpe <pedrofll@users.noreply.github.com>
2026-01-21 11:10:57 +0100

    Update compras.py

--- compras.py
+++ compras.py
@@ -433,7 +433,7 @@ def sincronizar(remotos):
             except Exception as e:
                 print(f"❌ Excepción durante la creación. Reintentando...", flush=True)
             
-            time.sleep(30)
+            time.sleep(60)
 
     hoy_fmt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"\n============================================================")
